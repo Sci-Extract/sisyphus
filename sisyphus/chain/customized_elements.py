60a2f92d910e496c72d2b11963f9b97a647571ad
soike lab <sukiluvcode@icloud.com>
2024-12-30 16:37:14 +0800

    finish inorganic extraction

--- sisyphus/chain/customized_elements.py
+++ sisyphus/chain/customized_elements.py
@@ -57,3 +57,49 @@ def customized_extractor(my_extractor: Callable[[Document], Optional[list[BaseMo
     if mode == 'normal':
         return do_extract_normal
     raise ValueError('mode should be one of "async", "thread" or "normal"')
+
+def customized_validator(my_validator: Callable[[DocInfo], Optional[DocInfo]], mode: Literal['async', 'thread', 'normal'], concurrent_number: int = 5) -> Callable:
+    """
+    customized validator
+
+    Args:
+        my_validator (Callable[[DocInfo], Optional[DocInfo]]): custom validator function
+        mode (Literal['async', 'thread', 'normal']): async, thread or just run one by one
+        concurrent_number (int, optional): maximum concurrently running tasks. Defaults to 5.
+
+    Returns:
+        Callable: a callable called by the chain, do not call it by yourself.
+    """
+    @return_valid
+    def do_validate_multi_threads(docinfos: list[DocInfo]) -> list[DocInfo]:
+        with ThreadPoolExecutor(max_workers=concurrent_number) as executor:
+            results = executor.map(my_validator, docinfos)
+        results = list(filter(None, results))
+        return results
+    
+    @return_valid
+    async def do_validate_async(docs: list[DocInfo]) -> list[DocInfo]:
+        def wrapped_with_sema(sema):
+            async def wrapped(docinfo):
+                async with sema:
+                    await my_validator(docinfo)
+            return wrapped
+        sema = asyncio.Semaphore(concurrent_number)
+        tasks = [wrapped_with_sema(sema)(doc) for doc in docs]
+        results = await asyncio.gather(*tasks)
+        results = filter(None, results)
+        return list(results)
+
+    @return_valid
+    def do_validate_normal(docs: list[DocInfo]) -> list[DocInfo]:
+        results = [my_validator(doc) for doc in docs]
+        results = filter(None, results)
+        return list(results)
+
+    if mode == 'async':
+        return do_validate_async
+    if mode == 'thread':
+        return do_validate_multi_threads
+    if mode == 'normal':
+        return do_validate_normal
+    raise ValueError('mode should be one of "async", "thread" or "normal"')
